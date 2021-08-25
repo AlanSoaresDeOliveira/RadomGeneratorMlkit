4a1c4e34e3bc2ed178a3cf88c3e0e3fb5a5a7c18
Alan Soares de Oliveira <alan.oliveira19@fatec.sp.gov.br>
2021-08-25 12:36:46 -0300

    create framework

---
+++ RadomGeneratorMlkit/RadomGeneratorMlkit/RadomGeneratorMlkit.h
@@ -0,0 +1,18 @@
+//
+//  RadomGeneratorMlkit.h
+//  RadomGeneratorMlkit
+//
+//  Created by Alan Soares on 25/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RadomGeneratorMlkit.
+FOUNDATION_EXPORT double RadomGeneratorMlkitVersionNumber;
+
+//! Project version string for RadomGeneratorMlkit.
+FOUNDATION_EXPORT const unsigned char RadomGeneratorMlkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RadomGeneratorMlkit/PublicHeader.h>
+
+
